4d36df9b852fcd80554b4bdeadaccb944d20a796
Aiiii-den <aiiden.dev@gmail.com>
2024-03-16 11:44:39 +0100

    abe: implemented add article functionality + small adjustment in edit-article

--- src/app/add-article/add-article.component.ts
+++ src/app/add-article/add-article.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from "../shared/article";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ApiRequestsService} from "../shared/api-requests.service";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-add-article',
@@ -6,5 +11,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent {
-  // TODO add API post request
+
+  id: string = '';
+  article: Article = {
+    _id: '',
+    title: '',
+    source: '',
+    link: '',
+    summary: '',
+    language: '',
+    topic: ''
+  };
+  form = new FormGroup({
+    titleControl: new FormControl<string>(''),
+    sourceControl: new FormControl<string>(''),
+    linkControl: new FormControl<string>(''),
+    summaryControl: new FormControl<string>(''),
+    languageControl: new FormControl<string>(''),
+    topicControl: new FormControl<string>(''),
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private as: ApiRequestsService,
+    private router: Router,
+    private location: Location,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  save(): void {
+    const values = this.form.value;
+    this.article.title = values.titleControl!;
+    this.article.source = values.sourceControl!;
+    this.article.link = values.linkControl!;
+    this.article.summary = values.summaryControl!;
+    this.article.topic = values.topicControl!;
+    this.article.language = values.languageControl!;
+    this.as.addEntry(this.article)
+      .subscribe({
+          next: (response) => {
+            console.log(response);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+          complete: () => console.log('save() completed')
+        }
+      );
+    this.router.navigateByUrl('/overview');
+  }
+
+  cancel(): void {
+    this.location.back();
+  }
+
+
 }
